a73595201fa1a533113bae5eaafa61b1c1a9866e
ssiniphachmratn-creator <ssiniphachmratn@gmail.com>
2026-07-06 14:55:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 const firebaseConfig = {
     apiKey: "YOUR_API_KEY",
     authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
+    projectId: "smartwater1-5e117",
     storageBucket: "YOUR_STORAGE_BUCKET",
     messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
     appId: "YOUR_APP_ID"
